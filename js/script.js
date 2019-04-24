71b00f680af1294b9947c838d37aced429085b34
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-04-24 03:01:56 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function staggerSubNav(navItem) {
+	var subNavTween = new TweenMax.staggerFromTo(navItem, 0.5, {x: '32px', autoAlpha: 0, ease: Power1.easeOut},{x: '0px', autoAlpha: 1}, 0.1);
+}
+$('nav > ul > li').on('mouseenter', function() {
+	staggerSubNav($(this).find('li'));
+});
