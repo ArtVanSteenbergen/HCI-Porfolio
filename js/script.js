e70197a2b82bb1f642cc91bd5fa3ff58af9680a9
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-04-24 11:16:57 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function staggerSubNav(navItem) {
-	var subNavTween = new TweenMax.staggerFromTo(navItem, 0.5, {x: '32px', autoAlpha: 0, ease: Power1.easeOut},{x: '0px', autoAlpha: 1}, 0.1);
+	var subNavTween = new TweenMax.staggerFromTo(navItem, 0.5, {x: '32px', autoAlpha: 0, ease: Power4.easeOut},{x: '0px', autoAlpha: 1}, 0.1);
 }
 $('nav > ul > li').on('mouseenter', function() {
 	staggerSubNav($(this).find('li'));
