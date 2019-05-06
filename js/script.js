5103902b1baebd5970acabaa1d78541e27e71567
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-05-06 15:22:27 +0200

    .

--- js/script.js
+++ js/script.js
@@ -8,4 +8,3 @@ $('nav > ul > li').on('mouseenter', function() {
 $( "code:contains('greenLedPin')" ).css( "color", "green" );
 $( "code:contains('yellowLedPin')" ).css( "color", "yellow" );
 $( "code:contains('redLedPin')" ).css( "color", "red" );
-$( "code:contains('delay')" ).css( "color", "lightblue" );
