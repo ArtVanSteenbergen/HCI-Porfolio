490ba12fc5e4a00d38dacb922e2e9a2ecb706199
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-05-06 14:05:45 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ function staggerSubNav(navItem) {
 $('nav > ul > li').on('mouseenter', function() {
 	staggerSubNav($(this).find('li'));
 });
+
+$( "code:contains('greenLedPin')" ).css( "color", "green" );
+$( "code:contains('yellowLedPin')" ).css( "color", "yellow" );
+$( "code:contains('redLedPin')" ).css( "color", "red" );
+$( "code:contains('delay')" ).css( "color", "lightblue" );
