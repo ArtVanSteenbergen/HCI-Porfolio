6b8aa16fdfbc2e17495e25c9ec2329ae6128a8aa
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-05-08 02:10:50 +0200

    Few updates to Arduino pages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
+var imgClicked = false;
+
 function staggerSubNav(navItem) {
 	var subNavTween = new TweenMax.staggerFromTo(navItem, 0.5, {x: '32px', autoAlpha: 0, ease: Power4.easeOut},{x: '0px', autoAlpha: 1}, 0.1);
 }
+
+$(document).on('click', function(event) {
+	if (event.target.className == 'breadboard') {
+		if (!imgClicked) {
+			imgClicked = true;
+			TweenMax.to(event.target, 1, {zIndex: 1000, maxWidth: '80vw', maxHeight: '80vh', position: 'fixed', left: '50%', top: '50%', x: '-50%', y: '-50%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0.8)', background: 'rgba(0,0,0,0.8)', ease: Power4.easeOut });
+		} else {
+			imgClicked = false;
+			TweenMax.to('.breadboard', 0.5, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)' });
+		}
+	} else {
+		if (imgClicked) {
+			imgClicked = false;
+			TweenMax.to('.breadboard', 0.5, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)' });
+		}
+	}
+});
+
 $('nav > ul > li').on('mouseenter', function() {
 	staggerSubNav($(this).find('li'));
 });
