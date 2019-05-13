a8faa563b0fbbdcbcf00b2dfb3427bab94ee4ea0
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-05-13 13:45:28 +0200

    changed a bunch

--- js/script.js
+++ js/script.js
@@ -11,18 +11,24 @@ $(document).on('click', function(event) {
 			TweenMax.to(event.target, 1, {zIndex: 1000, maxWidth: '80vw', maxHeight: '80vh', position: 'fixed', left: '50%', top: '50%', x: '-50%', y: '-50%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0.8)', background: 'rgba(0,0,0,0.8)', ease: Power4.easeOut });
 		} else {
 			imgClicked = false;
-			TweenMax.to('.breadboard', 0.5, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)' });
+			TweenMax.to('.breadboard', 0.3, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)', ease: Power4.easeOut});
 		}
 	} else {
 		if (imgClicked) {
 			imgClicked = false;
-			TweenMax.to('.breadboard', 0.5, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)' });
+			TweenMax.to('.breadboard', 0.3, {zIndex: 'initial', maxWidth: '128px', maxHeight: 'initial', position: 'absolute', left: 'initial', top: 'initial', x: '0%', y: '0%', boxShadow: '0px 0px 0px 9000px rgba(0,0,0,0)', background: 'rgba(0,0,0,0)', ease: Power4.easeOut });
 		}
 	}
 });
 
 $('nav > ul > li').on('mouseenter', function() {
 	staggerSubNav($(this).find('li'));
+});
+
+$('.fa-film').on('click', function(){
+	$('<img class="gif" src="' + $(this).data('gif') + '" />').insertAfter($(this));
+	TweenMax.fromTo($(this).next(), 0.5, {maxWidth: '64px', autoAlpha: 0, ease: Power4.easeOut},{maxWidth: '100%', autoAlpha: 1});
+	TweenMax.to($(this), 0.5, {autoAlpha: 0, fontSize: '0px', display: 'block',lineHeight: 0, ease: Power4.easeOut, onComplete: () => $(this).remove()});
 });
 
 $( "code:contains('greenLedPin')" ).css( "color", "green" );
